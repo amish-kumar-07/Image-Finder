cbca94878bb6b9d5c2312c819ebf948e76f6aba3
Amish kumar <157312626+amish-kumar-07@users.noreply.github.com>
2025-05-24 08:14:09 +0530

    third commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'], // <-- Add this line
+    domains: ['lh3.googleusercontent.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
